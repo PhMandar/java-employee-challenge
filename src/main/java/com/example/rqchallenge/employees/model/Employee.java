e2f0dd32763bad60b4b41647a8c3f20dd24fbafb
Mandar Phatak <mandar.m.phatak@gmail.com>
2024-10-09 18:20:04 +0530

    Refactorred the code

--- src/main/java/com/example/rqchallenge/employees/model/Employee.java
+++ src/main/java/com/example/rqchallenge/employees/model/Employee.java
@@ -1,35 +1,21 @@
 package com.example.rqchallenge.employees.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@Entity
-@Table(name = "employees")
 public class Employee {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @NotBlank(message = "Name cannot be blank")
-    @Column(name = "name")
     private String name;
 
     @NotNull(message = "Salary cannot be null")
     @Min(value = 0, message = "Salary must be a positive number")
-    @Column(name = "salary")
     private Double salary;
 
     @Min(value = 0, message = "Age must be a positive number")
-    @Column(name = "age")
     private Integer age;
 
     public Employee() {
--- src/test/java/com/example/rqchallenge/employees/service/ApiServiceTest.java
+++ src/test/java/com/example/rqchallenge/employees/service/ApiServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
